251fde8b4becb9a0bd018e44349f8db3e7d2db19
Prendota <Prendota@mail.ru>
2020-08-03 16:20:09 +0300

    chore(build): add lambda api

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,9 @@ repositories {
 dependencies {
   implementation(kotlin("stdlib-jdk8"))
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
+  api("com.amazonaws:aws-lambda-java-events:3.1.1")
+  api("com.amazonaws:aws-lambda-java-core:1.2.1")
+
 
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
