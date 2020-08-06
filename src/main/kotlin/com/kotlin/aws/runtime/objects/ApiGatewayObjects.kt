a4e5e85aab67e5a22d6c7bef3f17dd2a0124615b
Prendota <Prendota@mail.ru>
2020-08-06 23:39:03 +0300

    chore: add default & ignoreUnknown

--- src/main/kotlin/com/kotlin/aws/runtime/objects/ApiGatewayObjects.kt
+++ src/main/kotlin/com/kotlin/aws/runtime/objects/ApiGatewayObjects.kt
@@ -1,14 +1,17 @@
 package com.kotlin.aws.runtime.objects
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ApiGatewayProxyRequest(
-  var path: String,
-  var httpMethod: String,
-  var headers: Map<String, String>,
-  var queryStringParameters: Map<String, String>,
-  var body: String
+    var path: String = "",
+    var httpMethod: String = "GET",
+    var headers: Map<String, String> = emptyMap(),
+    var queryStringParameters: Map<String, String> = emptyMap(),
+    var body: String = ""
 )
 
 data class AwsLambdaInvocation(
-  val requestId: String,
-  val apiGatewayProxyRequest: ApiGatewayProxyRequest
+    val requestId: String,
+    val apiGatewayProxyRequest: ApiGatewayProxyRequest
 )
