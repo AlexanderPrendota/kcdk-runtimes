4e88aa789a15eb7938a969e0895510b125aca0df
Prendota <Prendota@mail.ru>
2020-08-10 22:57:11 +0300

    fix(env): port

--- src/main/kotlin/com/kotlin/aws/runtime/LambdaEnvironment.kt
+++ src/main/kotlin/com/kotlin/aws/runtime/LambdaEnvironment.kt
@@ -2,7 +2,7 @@ package com.kotlin.aws.runtime
 
 internal object LambdaRouters {
     private const val RUNTIME_DATE = "2018-06-01"
-    private val LOCAL_PORT = System.getenv("AWS_API_GATEWAY_PORT")?.toIntOrNull() ?: 8800
+    private val LOCAL_PORT = System.getenv("AWS_API_GATEWAY_PORT")?.toIntOrNull() ?: 8080
     private val RUNTIME_API = System.getenv("AWS_LAMBDA_RUNTIME_API") ?: "localhost:$LOCAL_PORT"
     private val RUNTIME_BASE_URL = "http://$RUNTIME_API/$RUNTIME_DATE/runtime"
 
