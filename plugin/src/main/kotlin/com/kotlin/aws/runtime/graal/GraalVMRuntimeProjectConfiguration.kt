6c58e5a43277d6e5bcd2aaad435093e3232b69e5
Prendota <Prendota@mail.ru>
2020-09-04 23:28:00 +0300

    refactor: create functions for graal

--- plugin/src/main/kotlin/com/kotlin/aws/runtime/graal/GraalVMRuntimeProjectConfiguration.kt
+++ plugin/src/main/kotlin/com/kotlin/aws/runtime/graal/GraalVMRuntimeProjectConfiguration.kt
@@ -12,39 +12,46 @@ import org.gradle.kotlin.dsl.get
 
 
 internal fun configureGraalVM(target: Project) {
-    target.pluginManager.apply("com.github.johnrengelman.shadow")
-    target.pluginManager.apply("com.bmuschko.docker-remote-api")
+    with(target) {
+        pluginManager.apply("com.github.johnrengelman.shadow")
+        pluginManager.apply("com.bmuschko.docker-remote-api")
+        val jar = createGraalJar()
+        val shadow = createShadowJarGraal(jar)
+        afterEvaluate {
+            //FIXME
+            // for some reason if we add generated sources via that command
+            // sources are ignored during compilation
+            target.mySourceSets.apply {
+                this["main"].java.srcDir(runtime.generationPath!!)
+            }
+        }
+        generateAdapter()
 
-    val jar = target.tasks.create("graalJar", Jar::class.java) {
+        //TODO disabled for now, since generated sources are ignored during compilation
+        // target.tasks["classes"].dependsOn(generateAdapter)
+        ConfigureGraal.apply(target, shadow)
+    }
+}
+
+private fun Project.generateAdapter() = tasks.create("generateAdapter", GenerateAdapter::class.java)
+
+private fun Project.createGraalJar(): Jar {
+    return tasks.create("graalJar", Jar::class.java) {
         it.group = Groups.build
         it.manifest { manifest ->
             manifest.attributes(mapOf("Main-Class" to "com.kotlin.aws.runtime.KotlinAWSCustomRuntimeKt"))
         }
     }
+}
 
-    val shadow = target.tasks.create("shadowJarGraal", ShadowJar::class.java) {
+private fun Project.createShadowJarGraal(jar: Jar): ShadowJar {
+    return tasks.create("shadowJarGraal", ShadowJar::class.java) {
         it.group = Groups.shadow
         it.archiveClassifier.set("graal")
         it.archiveVersion.set("")
-        it.from(target.mySourceSets["main"].output)
-        it.configurations.add(target.configurations["compileClasspath"])
-        it.configurations.add(target.configurations["runtimeClasspath"])
+        it.from(mySourceSets["main"].output)
+        it.configurations.add(configurations["compileClasspath"])
+        it.configurations.add(configurations["runtimeClasspath"])
         it.manifest.inheritFrom(jar.manifest)
     }
-
-    target.afterEvaluate {
-        //FIXME
-        // for some reason if we add generated sources via that command
-        // sources are ignored during compilation
-        target.mySourceSets.apply {
-            this["main"].java.srcDir(runtime.generationPath!!)
-        }
-    }
-
-   target.tasks.create("generateAdapter", GenerateAdapter::class.java)
-
-    //TODO disabled for now, since generated sources are ignored during compilation
-    // target.tasks["classes"].dependsOn(generateAdapter)
-
-    ConfigureGraal.apply(target, shadow)
 }
