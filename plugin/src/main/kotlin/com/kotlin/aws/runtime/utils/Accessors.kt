0797fa59b88c0d02b4b9beccd861c8ff935fa9a0
Prendota <Prendota@mail.ru>
2020-09-09 21:38:22 +0300

    refactor: make runtime as Project property

--- plugin/src/main/kotlin/com/kotlin/aws/runtime/utils/Accessors.kt
+++ plugin/src/main/kotlin/com/kotlin/aws/runtime/utils/Accessors.kt
@@ -3,9 +3,15 @@ package com.kotlin.aws.runtime.utils
 import org.gradle.api.Project
 import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.internal.HasConvention
+import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+internal inline fun <reified T : Any> Project.myExt(name: String) = myExt[name] as T
+
+internal val Project.myExt: ExtraPropertiesExtension
+    get() = myExtByName("ext")
 
 internal inline fun <reified T : Any> Project.myExtByName(name: String): T = extensions.getByName(name) as T
 
