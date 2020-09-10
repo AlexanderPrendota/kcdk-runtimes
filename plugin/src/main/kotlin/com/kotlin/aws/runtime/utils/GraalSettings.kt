2204a214ce3e1cc4809b7eaffb335a80b00f071c
Prendota <Prendota@mail.ru>
2020-09-10 19:43:15 +0300

    fix: correct flags sequence

--- plugin/src/main/kotlin/com/kotlin/aws/runtime/utils/GraalSettings.kt
+++ plugin/src/main/kotlin/com/kotlin/aws/runtime/utils/GraalSettings.kt
@@ -10,10 +10,10 @@ object GraalSettings {
         "--no-server",
         "-jar"
     )
-    val FULL_GRAAL_VM_FLAGS = BASE_GRAAL_FLAGS + listOf(
+    val FULL_GRAAL_VM_FLAGS = listOf(
         "-H:+AllowIncompleteClasspath",
         "-H:ReflectionConfigurationFiles=/working/build/${DEFAULT_REFLECT_FILE_NAME}",
         "-H:+ReportUnsupportedElementsAtRuntime",
         "--initialize-at-build-time=io.ktor,kotlinx,kotlin,org.apache.logging.log4j,org.apache.logging.slf4j,org.apache.log4j"
-    )
+    ) + BASE_GRAAL_FLAGS
 }
