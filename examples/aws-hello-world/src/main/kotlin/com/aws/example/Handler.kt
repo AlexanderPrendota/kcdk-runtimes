d46d4c7993bc3cdfc9842e2d8763289c1eb90eaa
Prendota <Prendota@mail.ru>
2020-09-24 20:28:55 +0300

    fix(example): correct handleRequest return type

--- examples/aws-hello-world/src/main/kotlin/com/aws/example/Handler.kt
+++ examples/aws-hello-world/src/main/kotlin/com/aws/example/Handler.kt
@@ -1,12 +1,11 @@
 package com.aws.example
 
 import com.amazonaws.services.lambda.runtime.Context
-import com.kotlin.aws.runtime.objects.LambdaContext
 import java.io.InputStream
 import java.io.OutputStream
 
 class Handler {
-    fun handleRequest(inputStream: InputStream, outputStream: OutputStream, context: Context): String {
-        return "Hello from graal lambda!"
+    fun handleRequest(inputStream: InputStream, outputStream: OutputStream, context: Context) {
+        outputStream.write("Hello world".toByteArray())
     }
 }
