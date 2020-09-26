b4fa35590c8159639f40a7d2af8cd18961b0764b
Prendota <Prendota@mail.ru>
2020-09-26 12:59:12 +0300

    refactor(js): remove unused

--- plugins/plugin-js/src/main/kotlin/com/kotlin/aws/js/runtime/utils/Extensions.kt
+++ plugins/plugin-js/src/main/kotlin/com/kotlin/aws/js/runtime/utils/Extensions.kt
@@ -6,11 +6,3 @@ import org.gradle.api.Task
 
 internal val Project.runtime
     get() = this.extensions.findByName("runtime") as RuntimePluginExtension? ?: this.extensions.create("runtime", RuntimePluginExtension::class.java)
-
-internal fun Project.getTask(name: String): Task {
-    val task = this.tasks.findByName(name)
-    require(task != null) {
-        "Kotlin JS Runtime can not find task '$name'"
-    }
-    return task
-}
