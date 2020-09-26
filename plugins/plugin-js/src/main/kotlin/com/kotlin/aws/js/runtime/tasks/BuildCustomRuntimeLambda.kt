17b88570c3114a065557800423a69b91021dc9a4
Prendota <Prendota@mail.ru>
2020-09-26 13:04:39 +0300

    refactor(js): BuildCustomRuntimeLambda

--- plugins/plugin-js/src/main/kotlin/com/kotlin/aws/js/runtime/tasks/BuildCustomRuntimeLambda.kt
+++ plugins/plugin-js/src/main/kotlin/com/kotlin/aws/js/runtime/tasks/BuildCustomRuntimeLambda.kt
@@ -1,5 +1,6 @@
 package com.kotlin.aws.js.runtime.tasks
 
+import com.kotlin.aws.js.runtime.dsl.RuntimePluginExtension
 import com.kotlin.aws.js.runtime.utils.Groups
 import com.kotlin.aws.js.runtime.utils.runtime
 import org.gradle.api.DefaultTask
@@ -10,7 +11,7 @@ import java.io.File
 open class BuildCustomRuntimeLambda : DefaultTask() {
 
     @get:Input
-    val extension = project.runtime
+    val extension: RuntimePluginExtension = project.runtime
 
     init {
         group = Groups.aws
@@ -19,17 +20,16 @@ open class BuildCustomRuntimeLambda : DefaultTask() {
     @TaskAction
     fun build() {
         val outputDirPath = extension.outputDir
-        val dir = if (outputDirPath != null)
-            File(outputDirPath)
-        else
-            File(project.buildDir, "distributions")
+        val dir = outputDirPath?.let { File(it) } ?: File(project.buildDir, "distributions")
 
         dir.mkdirs()
         with(File(dir, "bootstrap")) {
-            writeText("""
+            writeText(
+                """
                 # just a placeholder, not tested yet
                 node sample.js
-            """.trimIndent())
+            """.trimIndent()
+            )
         }
     }
 
