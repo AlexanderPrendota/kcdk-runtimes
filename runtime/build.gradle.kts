cd0243f0f716e36fade6bd0995bd77ae564e755d
Prendota <Prendota@mail.ru>
2020-10-08 22:08:08 +0300

    chore(log): add slf4j

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     api(kotlin("stdlib"))
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
+    api("org.slf4j", "slf4j-api", "1.7.30")
     api("com.amazonaws:aws-lambda-java-events:3.1.1")
     api("com.amazonaws:aws-lambda-java-core:1.2.1")
 
