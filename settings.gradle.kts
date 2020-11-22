c92260e3ca34a0d40e036b4af02cae3eb66db9f1
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-11-22 16:34:45 +0700

    Move plugin versions to gradle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,17 @@
 rootProject.name = "kotlin-aws-lambda-custom-runtimes"
 
+pluginManagement {
+    val kotlinVersion: String by settings
+    val kosogorVersion: String by settings
+    val detektVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        id("tanvd.kosogor") version kosogorVersion
+        id("io.gitlab.arturbosch.detekt") version detektVersion
+    }
+}
+
 include(":runtimes:runtime-graalvm")
 include(":plugins:plugin-graalvm")
 include(":plugins:plugin-js")
